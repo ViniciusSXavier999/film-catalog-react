28c2ba63a8b89cb5e2bdaaa4b867622052ef2a20
ViniciusSXavier999 <viniciusxaviersouza11@gmail.com>
2024-05-10 12:36:11 -0300

    Definindo variaveis de ambientes e as rotas da aplicação

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import './index.css'
+import App from './App'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route element= {<App/>}>
+          <Route path='/' element={<Home />} />
+          <Route path='movie/:id' element={<Movie />} />
+          <Route path='search' element={<Search />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
